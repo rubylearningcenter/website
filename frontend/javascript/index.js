46ba615b09da77b930d28cfd60d5bb78ed031b04
Brandon Weaver <keystonelemur@gmail.com>
2022-09-14 21:14:11 -0700

    clean install - ERB and Sass options

---
+++ frontend/javascript/index.js
@@ -0,0 +1,7 @@
+import "index.scss"
+import "syntax-highlighting.css"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
+
+console.info("Bridgetown is loaded!")
